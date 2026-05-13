0bbddf5d4e3b2987bae916c80912be7aa711e333
sherjama <sherjama8769214377@gmail.com>
2026-05-13 11:15:53 +0530

    Deployment Ready

--- config.py
+++ config.py
@@ -7,11 +7,6 @@ class Config:
 
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('DB_USER')}:"
-        f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}/"
-        f"{os.getenv('DB_NAME')}"
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
